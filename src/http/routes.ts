a22195cb09a811a82f44991d851b97734526234e
andreScheffer <andre.scheffer@ctjunior.com.br>
2025-07-13 09:34:58 -0300

    editor de perfil

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { register } from "./controllers/register.controller";
 import { authenticate } from "./controllers/authenticate.controller";
+import { profile } from "./controllers/profile.controller";
 
 export async function appRoutes(app: FastifyInstance) {
     app.post('/user', register)
     app.post('/login', authenticate)
+    app.patch('/user', profile)
 }
