5c4fae8feb02de2ab9b7efb0ac9b84b4ae3d1eb5
andreScheffer <andre.scheffer@ctjunior.com.br>
2025-07-13 11:01:24 -0300

    criar post

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -3,6 +3,7 @@ import { register } from "./controllers/register.controller";
 import { authenticate } from "./controllers/authenticate.controller";
 import { profile } from "./controllers/profile.controller";
 import { criarPost } from "./controllers/criarPost.controller";
+import { editarPost } from "./controllers/editarPost.controller";
 
 export async function appRoutes(app: FastifyInstance) {
     app.post('/user', register)
@@ -10,4 +11,5 @@ export async function appRoutes(app: FastifyInstance) {
     app.patch('/user', profile)
 
     app.post('/post', criarPost)
+    app.patch('/post:id', editarPost)
 }
