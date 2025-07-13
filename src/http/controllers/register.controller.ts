c053cef338da85ec3fc47241d9c548f0d9860b57
andreScheffer <andre.scheffer@ctjunior.com.br>
2025-07-13 15:44:17 -0300

    use cases erros

--- src/http/controllers/register.controller.ts
+++ src/http/controllers/register.controller.ts
@@ -19,7 +19,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply){
     })
   } catch (err) {
     if(err instanceof UserAlreadyExistsError){
-      return reply.status(409).send({ message: err.message })
+      return reply.status(409).send();
     }
 
     throw err
